afb553113a5971001c519aa476fa9068690bc57e
yashkamble <ykamble20comp@student.mes.ac.in>
2023-07-15 12:59:18 +0530

    Recursion-Subset-subsequence

---
+++ Recursion/Subset-Subsequence-String/AsciiValueOfCharacter.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class AsciiValueOfCharacter {
+    public static void main(String[] args) {
+        char ch = 'a';
+        subseqAscii("", "abc");
+    }
+    static void subseqAscii(String processed , String unprocessed){
+        if(unprocessed.isEmpty()){
+            System.out.println(processed);
+            return;
+        }
+        subseqAscii(processed + unprocessed.charAt(0), unprocessed.substring(1));
+        subseqAscii(processed, unprocessed.substring(1));
+        subseqAscii(processed + (unprocessed.charAt(0)+0) , unprocessed.substring(1));
+    }
+}
--- Recursion/Subset-Subsequence-String/subsequence.java
+++ Recursion/Subset-Subsequence-String/subsequence.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class subsequence {
     public static void main(String[] args) {
         subseq("", "abc");
+        System.out.println(subseqArraylist("", "abc"));
     }
     static void subseq(String processed , String unprocessed){
         if(unprocessed.isEmpty()){
@@ -9,5 +12,18 @@ public class subsequence {
         }
         subseq(processed + unprocessed.charAt(0), unprocessed.substring(1));
         subseq(processed, unprocessed.substring(1));
+    }
+    // Returning the ArrayList of String :
+    static ArrayList<String> subseqArraylist(String processed , String unprocessed){
+        if(unprocessed.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+        ArrayList<String> left =  subseqArraylist(processed + unprocessed.charAt(0), unprocessed.substring(1));
+        ArrayList<String> right =  subseqArraylist(processed, unprocessed.substring(1));
+
+        left.addAll(right);
+        return left;
     }
 }
